04b975cc8c09c98e33a85c6797345cc9d3cae810
MatheuzH <110929706+MatheuzH@users.noreply.github.com>
2023-10-16 15:47:53 -0300

    Update main.go

--- main.go
+++ main.go
@@ -7,9 +7,9 @@ import (
 	"os"
 )
 
-<<<<<<< HEAD
+
 var listaDeProdutos []produtos.Produto
-var proximoId = 0
+var proximoId = 1
 var totalPedExpedidos = 0
 
 // var tempoExpedidoTotal time.Time
@@ -30,10 +30,6 @@ type Pedido struct {
 	Status     bool
 }
 
-var produtos []Produto
-var proximoId = 1
->>>>>>> aec3b74d5cdde5415d8e0746f99ba592e47eb5d8
-
 func main() {
 	operacoes.LerProdAdcLista()
 	for {
@@ -49,7 +45,7 @@ func main() {
 		fmt.Scanln(&escolha)
 
 		switch escolha {
-<<<<<<< HEAD
+			
 		case 1: // cadastra
 			if len(listaDeProdutos) < 50 {
 				operacoes.CadastraProduto()
@@ -70,45 +66,12 @@ func main() {
 			operacoes.RemoveProdutoPorID(idParaRemover)
 
 		case 5:
-=======
-			case 1: // cadastra
-				if len(produtos) < 50 {
-					cadastraProduto()
-				} else {
-					fmt.Println("Limite de 50 produtos atingido!")
-				}
-
-			case 2: //visualiza produtos
-				produtosCadastrados()
-
-			case 3: //busca pelo id
-				fmt.Println("Digite o ID do produto:")
-				scanner := bufio.NewScanner(os.Stdin)
-				scanner.Scan()
-				produtoID, err := strconv.Atoi(scanner.Text())
-				if err != nil {
-					fmt.Println("O id necessita ser um inteiro!")
-				}else{
-					fmt.Printf("Produto Encontrado:\nID: %d, Nome: %s, Descrição: %s, Valor: %.2f, Quantidade: %d\n", buscaProduto(produtoID).ID, buscaProduto(produtoID).Nome, buscaProduto(produtoID).Descricao, buscaProduto(produtoID).Valor, buscaProduto(produtoID).Quantidade)
-				}
-			case 4: // remover um produto
-				fmt.Print("Digite o ID do produto que você deseja remover: ")
-				var idParaRemover int
-				fmt.Scanln(&idParaRemover)
-				removeProdutoPorID(idParaRemover)
-
-		case 6:
->>>>>>> aec3b74d5cdde5415d8e0746f99ba592e47eb5d8
 			// remover todos os produtos
 			listaDeProdutos = nil
 			fmt.Println("Todos os produtos foram removido")
 
-<<<<<<< HEAD
 		case 6:
 			operacoes.BuscarProdutoNome()
-=======
-		
->>>>>>> aec3b74d5cdde5415d8e0746f99ba592e47eb5d8
 
 			/*
 				case 7:
@@ -129,160 +92,9 @@ func main() {
 	}
 
 
-<<<<<<< HEAD
-=======
-func cadastraProduto() {
-	scanner := bufio.NewScanner(os.Stdin)
-
-	for {
-		novoProduto := Produto{}
-
-		fmt.Print("Nome do produto (digite -1 para sair): ")
-		scanner.Scan()
-		nome := scanner.Text()
-
-		if nome == "-1" {
-			break
-		}
-		novoProduto.Nome = nome
-
-		novoProduto.ID = proximoId
-		proximoId++
-
-		fmt.Print("Descrição do produto: ")
-		scanner.Scan()
-		novoProduto.Descricao = scanner.Text()
-
-		fmt.Print("Valor do produto: ")
-		scanner.Scan()
-		valorStr := scanner.Text()
-		valor, err := strconv.ParseFloat(valorStr, 64) // tratamento de exceção - valor
-		if err != nil {
-			fmt.Println("Erro ao ler valor. Certifique-se de digitar um número válido.")
-			fmt.Println("-------------------------------------------------------------")
-			continue
-		}
-		novoProduto.Valor = valor
-
-		fmt.Print("Quantidade do produto: ")
-		scanner.Scan()
-		quantidadeStr := scanner.Text()
-		quantidade, err := strconv.Atoi(quantidadeStr) // tratamento de exceção - quantidade
-		if err != nil {
-			fmt.Println("Erro ao ler quantidade. Certifique-se de digitar um número válido.")
-			fmt.Println("-------------------------------------------------------------")
-			continue
-		}
-		novoProduto.Quantidade = quantidade
-
-		fmt.Println("---------------------------------")
-
-		produtos = append(produtos, novoProduto)
-	}
-}
-
-func produtosCadastrados() {
-	fmt.Println("Produtos cadastrados:")
-	for _, p := range produtos {
-		fmt.Printf("ID: %d, Nome: %s, Descrição: %s, Valor: %.2f, Quantidade: %d\n", p.ID, p.Nome, p.Descricao, p.Valor, p.Quantidade)
-	}
-}
-
-func buscaProduto(produtoId int)Produto{
-	//para encontrar o produto pelo id
-	for _, p := range produtos {
-		if produtoId == p.ID{
-			return p
-		} else {
-			fmt.Println("Não achamos o produto!")
-			break
-		}
-	}
-}
-
-func removeProdutoPorID(id int) {
-	for i, produto := range produtos {
-		if produto.ID == id {
-			produtos = append(produtos[:i], produtos[i+1:]...)
-			fmt.Printf("Produto com ID %d removido com sucesso!\n", id)
-			return
-		}
-	}
-	fmt.Printf("Produto com ID %d não encontrado.\n", id)
-}
-
-func AdicionaPedido(id int,novoPedido Pedido) {
-	for _, p := range produtos {
-		if id == p.ID {
-			if p.Quantidade == 0 {
-				fmt.Println("O produto está sem estoque")
-				proximoIdPedido--
-			}else{
-				novoPedido.ValorTotal = novoPedido.ValorTotal + p.Valor
-				novoPedido.Produtos[i] = p
-				i++
-				p.Quantidade--
-			}
-		} else {
-			fmt.Println("Nenhum produto encotrado com esse ID")
-		}
-	}
-}
-
-
-
-func expedirPedidos(){
-	
-}
 
 //EXTRAS
 
-func LerProdAdcLista() {
-	// Abrir o arquivo texto
-	file, err := os.Open("ProdAdcLista.csv")
-	if err != nil {
-		fmt.Println("Erro ao abrir arquivo ProdAdcLista.csv")
-		return
-	}
-	defer file.Close()
-
-	// Criar um leitor para o arquivo
-	reader := csv.NewReader(file)
-	for {
-		linha, err := reader.Read()
-		if err != nil {
-			break
-		}
-
-		//cria novo produto
-		novoProduto := Produto{}
-
-		novoProduto.ID = proximoId
-		proximoId++
-
-		novoProduto.Nome = linha[0]
-		novoProduto.Descricao = linha[1]
-
-		valor64, err1 := strconv.ParseFloat(linha[2], 64)
-		if err1 != nil {
-			fmt.Println("Erro ao converter valor para float64")
-			return
-		}
-		novoProduto.Valor = valor64
-
-		qtdInt, err2 := strconv.Atoi(linha[3])
-		if err2 != nil {
-			fmt.Println("Erro ao converter valor para inteiro")
-			return
-		}
-		novoProduto.Quantidade = qtdInt
-
-		//Adiciona os produtos na lista
-		produtos = append(produtos, novoProduto)
-	}
-}
-
->>>>>>> aec3b74d5cdde5415d8e0746f99ba592e47eb5d8
 /*
 
 func ExpedirPedido(){
